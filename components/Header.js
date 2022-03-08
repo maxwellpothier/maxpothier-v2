ee4d17bf24da4e9dfc1ec893cc932dbbbe53c0a8
maxwellpothier <pothiermaxwell@gmail.com>
2022-03-08 00:44:03 -0700

    changes some netlify build errors

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
         <div className={styles.headerContainer}>
             <span className={styles.headerLogo}>What's Turning?</span>
             <ul className={styles.headerSocialIcons}>
-                <a href="https://open.spotify.com/user/bg8527p8wb1nrpb6r8xf662r2?si=a32cb6c1e3214d3a" target={"_blank"}>
+                <a href="https://open.spotify.com/user/bg8527p8wb1nrpb6r8xf662r2?si=a32cb6c1e3214d3a" target={"_blank"} rel={"noreferrer"}>
                     <FaSpotify className={styles.headerIcon}/>
                 </a>
-                <a href="https://github.com/maxwellpothier" target={"_blank"}>
+                <a href="https://github.com/maxwellpothier" target={"_blank"} rel={"noreferrer"}>
                     <FaGithub className={styles.headerIcon}/>
                 </a>
-                <a href="https://twitter.com/MaxPothier" target={"_blank"}>
+                <a href="https://twitter.com/MaxPothier" target={"_blank"} rel={"noreferrer"}>
                     <FaTwitter className={styles.headerIcon}/>
                 </a>
             </ul>
