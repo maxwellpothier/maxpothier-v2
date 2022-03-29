b76689f592d69ff77ed4092c7cae39971e9fb8d7
maxwellpothier <pothiermaxwell@gmail.com>
2022-03-29 11:02:43 -0600

    adding content to the review section

--- components/ReviewContent.js
+++ components/ReviewContent.js
@@ -1,13 +1,24 @@
-import PageHeader from "./headers/PageHeader";
+import ReviewParagraph from "./ReviewParagraph";
+import Image from "next/image";
 
 import styles from "./reviewContent.module.scss";
 
 const ReviewContent = () => {
     return (
         <div className={styles.reviewContentContainter}>
-            <div className={styles.reviewContentInfo}>
+            <div className={styles.reviewContentCover}>
+                <Image
+                        src={"https://i.scdn.co/image/ab67616d00001e02932b996af72dfeaf5a72ef58"}
+                        alt={"Test Photo"}
+                        width={200}
+                        height={200}
+                />
             </div>
             <div className={styles.reviewContentText}>
+                <ReviewParagraph/>
+            </div>
+            <div className={styles.reviewContentGrade}>
+                <span>9.2</span>
             </div>
         </div>
     );
